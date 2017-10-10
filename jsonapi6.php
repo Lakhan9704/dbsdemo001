c52cbf881c0ef94994a7fa555aaffb58e006cd14
Lakhan9704 <32671022+Lakhan9704@users.noreply.github.com>
2017-10-10 13:29:52 +0100

    Update jsonapi6.php

--- jsonapi6.php
+++ jsonapi6.php
@@ -2,9 +2,9 @@
  $items = array();
  
  for($i = 0; $i < 10; $i++) {
- $items[$i] = " this is message $i";
+ $items[$i] = "This is message $i";
  }
- header('content-Type: application/json');
+ header('Content-Type: application/json');
  
  $jsonOutput = json_encode($items);
  
